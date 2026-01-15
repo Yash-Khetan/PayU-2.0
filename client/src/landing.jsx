bd74a25823a47d83413dd1d13f865b8bda5bb12f
Yash-Khetan <yash.khetan24@spit.ac.in>
2026-01-15 23:58:56 +0530

    added the landing page and some changes in app.jsx

---
+++ client/src/landing.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+import { useState } from 'react';
+
+export const landing =() => {
+    return  <div>
+        <h1>Welcome to the Landing Page</h1>
+        <p>This is a sample landing page for the payment application.</p>
+    </div>
+}
